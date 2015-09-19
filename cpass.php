84d122f28aff0c2e0f45a608a56a7ba5da1b03c9
Cristóbal Águila Rebolledo <cristobal.aguila@gmail.com>
2015-09-19 14:53:36 -0300

    Added Functions
    Emails the new password to the alternative email account.

--- cpass.php
+++ cpass.php
@@ -43,8 +43,49 @@ $f = curl_exec($ch);
 // close cURL resource, and free up system resources
 curl_close($ch);
 
+$sql="SELECT * FROM usuarios WHERE usuario = '$email';";
+$result = mysqli_query($con,$sql);
+if ($result->num_rows > 0){
+while($row = mysqli_fetch_array($result))
+  {
+  $destino = $row['correo_alt']
+  $epass = $pass;
+  }
+  }
+// !!! **********Fin Correo************* !!! //
+	//enviamos el correo
+	if ($destino != "@"){
+		$eol = PHP_EOL;
+		$mensaje = "Se ha modificado su contraseña. ".$eol." Le recordamos que para revisar su bandeja de entrada deberá ingresar a ".$urlwebmail." e ingresar los siguientes datos: ".$eol." Correo: ".$euser."@".$edomain."".$eol." Contraseña: ".$epass."".$eol." Ante cualquier problema puede escribir a correos@".$edomain."".$eol." Muchas gracias.";
+		$body = wordwrap($mensaje, 70, "\r\n");
+		$headers = 'From: correos@'.$edomain.$eol;
+    		$asunto = 'Se ha creado una cuenta de correo electrónico para usted';
+    		
+    		// empezamos a armar el mensaje \\
+$emsg = "Content-Type: multipart/alternative".$eol;
+
+// En caso de que no podamos leer html \\
+$emsg .= "--".$random_hash.$eol;
+$emsg .= "Content-Type: text/plain; charset=iso-8859-1".$eol;
+$emsg .= "Content-Transfer-Encoding: 7bit".$eol;
+$emsg .= "Este e-mail requiere que active HTML. ".$eol;
+$emsg .= "Si usted está leyendo esto, por favor actualice su cliente de correo. ".$eol;
+$emsg .= "------- Mensaje truncado -------".$eol.$eol;
+
+// Lo "normal", que podamos leer html \\
+$emsg .= "--".$random_hash.$eol;
+$emsg .= "Content-Type: text/html; charset=iso-8859-1".$eol;
+$emsg .= "Content-Transfer-Encoding: 7bit".$eol;
 
 
+$emsg = $body.$eol.$eol; // !!! ***********************
+
+  
+  
+		mail($destino, $asunto, wordwrap($emsg,70,$eol),$headers);
+		
+		// !!! **********Fin Correo************* !!! //
+}
 header('Location: cpemail.php');
 die();
 ?>
